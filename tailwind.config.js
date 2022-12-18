b6ed765810c3dee574418ab16a0f3cac8c7fdb96
Amir Hossein <amirclashroyal09@gmail.com>
2022-12-18 17:07:08 +0330

    Config Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'Vazirmatn','sans-serif'],
+      },
+      width: {
+        '35r': '35rem'
+      },
+      height: {
+        '35r': '35rem'
+      },
+      colors: {
+        'lightgray': '#BDBDBD',
+        'lightgreen': '#28CDBA',
+      }
+    },
+  },
+  plugins: [],
+}
